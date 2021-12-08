32a2c553f00bcfe0839f7b491ed7e462f7f68865
Daphne Carvalho <dah.feh@hotmail.com>
2021-12-08 11:54:51 -0300

    Updating seeds file.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -14,8 +14,7 @@ alias Exmeal.{Meal, Repo}
 meal = %Meal{
   description: "Potato Chips",
   calories: 200,
-  date: ~N[2021-12-05 03:05:24],
-  id: "2423e0d9-9d0d-4883-bce1-4d625cb92c2e"
+  date: ~N[2021-12-05 03:05:24]
 }
 
 Repo.insert!(meal)
